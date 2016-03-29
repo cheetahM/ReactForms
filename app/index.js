37244e407b8fca68dccf4b48f8419d9df587b108
chmeshra <chmeshra@visa.com>
2016-03-28 23:11:23 -0700

    proect setup with webpack configs

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*var Hello = React.createClass({
+  render: function() {
+    return (
+      <div>Hello!!</div>
+    )
+  }
+})*/
+
+function Hello(){
+  return (
+    <div>Hello World!!</div>
+  )
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
