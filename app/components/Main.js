28deca3db7004b967af918af6d568474b2d0025d
cmeshram <meshram.chetan@gmail.com>
2016-04-08 23:47:38 -0700

    changing main container to flexbox container

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Main = React.createClass({
   render: function() {
     return (
-      <div className="main-container">
+      <div className="flex-container">
         {this.props.children}
       </div>
     )
